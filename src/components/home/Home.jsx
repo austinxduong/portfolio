dc5d85e44d038739deef316d18fff85f55fc2fd9
austinxduong <axduong2@gmail.com>
2021-08-19 11:21:13 -0700

    install react-router-dom

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Home.css';
+
+const Home = () => {
+  return (
+    <main className={styles.Home}>
+      <h1>austi</h1>
+    </main>
+  );
+};
+
+export default Home;
